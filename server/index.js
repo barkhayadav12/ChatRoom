e1447b684fe58c53398795a460066a8a0137ae69
barkhayadav12 <143165059+barkhayadav12@users.noreply.github.com>
2023-12-20 16:42:52 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ const {Server}=require('socket.io');
 app.use(cors());
 const io=new Server(server,{
     cors:{
-        origin:'http://localhost:5173',
+        origin:'https://chat-room-ashen.vercel.app',
         methods:["GET","POST"],
     }
 });
